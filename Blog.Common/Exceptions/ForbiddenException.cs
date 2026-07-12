baff153f8fba68a2fa4bcfd540d27ef61ec07249
Muhammadaliyev Husanjon <120047912+HusanjonDeveloper@users.noreply.github.com>
2026-07-12 21:30:57 +0500

    ForbiddenException

---
+++ Blog.Common/Exceptions/ForbiddenException.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Common.Exceptions;
+
+// Foydalanuvchi autentifikatsiyadan o'tgan, lekin bu amalni bajarishga ruxsati yo'q
+// (masalan, boshqa birovning blogini o'chirmoqchi bo'lsa).
+// Global middleware buni HTTP 403 statusiga aylantiradi.
+public class ForbiddenException : Exception
+{
+    public ForbiddenException(string message) : base(message)
+    {
+    }
+}
